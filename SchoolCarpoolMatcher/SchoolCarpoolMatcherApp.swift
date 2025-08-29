21da57d4589825bff2e927ee497cfa8d3341ca92
tashi tsering <tatsering54@gmail.com>
2025-08-30 00:35:48 +1000

    Initial Commit

---
+++ SchoolCarpoolMatcher/SchoolCarpoolMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolCarpoolMatcherApp.swift
+//  SchoolCarpoolMatcher
+//
+//  Created by Tashi Tsering on 30/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct SchoolCarpoolMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
